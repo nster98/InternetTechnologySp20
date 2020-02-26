394e373ddc5edc5c7f60d88692796b0e3825c750
Nster98 <nster98@gmail.com>
2020-02-26 17:38:11 -0500

    Added a dictionary to the RS and TS server

--- project1/rs.py
+++ project1/rs.py
@@ -4,6 +4,22 @@
 
 import socket as mysoc
 
+# Set up DNS Table
+
+fd = open("PROJ1-DNSRS.txt", "r")
+
+num_lines = sum(1 for line in open('PROJ1-DNSRS.txt'))
+dnsTable = [dict(hostname = "", ip = "", flag = "") for x in range(num_lines)]
+
+# TODO: Possible error with the \n being written into the dictionary
+
+for i in range(num_lines):
+	string = fd.readline()
+	data = string.split(' ')
+	dnsTable[i] = dict(hostname = data[0], ip = data[1], flag = data[2])
+
+# print(dnsTable)
+
 # Set up server socket
 
 try:
@@ -13,9 +29,21 @@ except mysoc.error as err:
 	print('{} \n'.format("Error creating RS socket", err))
 
 # Bind RS server to port 69696 and enable listening
-server_binding = ('', 69696)
+server_binding = ('', 50006)
 rs.bind(server_binding)
 rs.listen(1)
 
 # Wait for client to connect
 clientid, addr = rs.accept()
+
+while True:
+
+	clientQuery = rs.recv(1024).decode('utf-8')
+	
+	# Check against table to see what we got
+	# TODO: Make sure its sending correctly when there is an error and cant find any server, or just the TS server 
+	for i in range(num_lines):
+		if clientQuery == dnsTable[i]["hostname"]:
+			returnString = dnsTable[i]["hostname"] + " " + dnsTable[i]["ip"] + " " + dnsTable[i]["flag"]
+		
+	rs.send(returnString.encode('utf-8'))
--- project1/ts.py
+++ project1/ts.py
@@ -4,6 +4,22 @@
 
 import socket as mysoc
 
+# Set up DNS Table
+
+fd = open("PROJ1-DNSTS.txt", "r")
+
+num_lines = sum(1 for line in open('PROJ1-DNSTS.txt'))
+dnsTable = [dict(hostname = "", ip = "", flag = "") for x in range(num_lines)]
+
+# TODO: Possible error with the \n being written into the dictionary
+
+for i in range(num_lines):
+	string = fd.readline()
+	data = string.split(' ')
+	dnsTable[i] = dict(hostname = data[0], ip = data[1], flag = data[2])
+
+print(dnsTable)
+
 # Set up server socket
 
 try:
@@ -13,9 +29,11 @@ except mysoc.error as err:
 	print('{} \n'.format("Error creating TS socket", err))
 
 # Bind RS server to port 69696 and enable listening
-server_binding = ('', 69696)
+server_binding = ('', 50007)
 ts.bind(server_binding)
 ts.listen(1)
 
 # Wait for client to connect
 clientid, addr = ts.accept()
+
+
