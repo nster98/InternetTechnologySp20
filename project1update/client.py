f68a0496829a8add6d0abf3974e0b189a9db112c
Nster98 <nster98@gmail.com>
2020-02-29 15:43:08 -0500

    Trying to put TS connection elsewhere

--- project1update/client.py
+++ project1update/client.py
@@ -13,12 +13,6 @@ import sys
 def client():
 		if len(sys.argv) != 4:
 			print("ERROR: PLEASE INPUT THE RS SERVER AND 2 PORTS")
-
-		try:
-			ts_socket = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
-			print("DEBUG: Client ts socket created\n")
-		except mysoc.error as err:
-			print('{} \n'.format("Client ts socket open error", err))
 	
 		try:
 			rs_socket = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
@@ -38,39 +32,67 @@ def client():
 		# TODO: Move TS server binding and connect to after needing to connect to it
 
 		server_binding = (sys.argv[1], port)
-		server_binding2 = (sa_sameas_myaddr, port2)
-		ts_socket.connect(server_binding2)
+		#server_binding2 = (sa_sameas_myaddr, port2)
+		#ts_socket.connect(server_binding2)
 		rs_socket.connect(server_binding)
 
 		inputFile = open(os.path.join(sys.path[0],"PROJI-HNS.txt"),"r")
+
 		if inputFile.mode == 'r':
+
+				# Set up query to send to RS server
 				inputList = [i.rstrip() for i in inputFile]
 				inputString = '_'.join([str(j) for j in inputList])
 				query = inputString.lower()
+
+				# Send query to RS server
 				rs_socket.send(query.encode('utf-8'))
+
+				# Recieve response from RS server in form <hostname ip flag>
 				returnQuery = rs_socket.recv(1024).decode('utf-8')
+
+				# Split each part by spaces and put in list
 				listData = returnQuery.split('_')
  				outputList = ["NONE"]*len(listData)
 				tsList = []
+
+				# Loop through data and check for A or NS
+				# If NS, use hostname to set up TS socket and connect to it
 				for i in range(0, len(listData)):
 						tempList = listData[i].split(' ')
+						print(tempList)
 						if tempList[2] == 'A':
 								outputList[i] = listData[i]
 						else:
 								tsList.append(str(i)+' '+listData[i])
+
+								# Set up TS socket here
+
+								try:
+									ts_socket = mysoc.socket(mysoc.AF_INET, mysoc.SOCK_STREAM)
+									print("DEBUG: Client ts socket created\n")
+								except mysoc.error as err:
+									print('{} \n'.format("Client ts socket open error", err))
+
+								newAddr = mysoc.gethostbyname(tempList[0])
+								server_binding2 = (newAddr, port2)
+								ts_socket.connect(server_binding2)
+
 				if len(tsList)>0:
+
 						tsString = '_'.join([str(j) for j in tsList])
 						ts_socket.send(tsString.encode('utf-8'))
+
 						returnQuery2 = ts_socket.recv(1024).decode('utf-8')
+
 						returnList = returnQuery2.split('_')
+
 						for i in range(0, len(returnList)):
 								tempList = returnList[i].split(' ')
 								if tempList[3] == 'Error':
 									tempList[3] = 'Error:HOST NOT FOUND'
 								outputList[int(tempList[0])] = (tempList[1]+' '+tempList[2]+' '+tempList[3])
 
-				# print(outputList)
-				
 				outputFile = open("RESOLVEDTEST.txt", "w")
 				
 				for i in range(0, len(outputList)):
